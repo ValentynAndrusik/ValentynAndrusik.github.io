d03356a84a09370e3346cb58003df276771a03e3
Valentyn Andrusik <44110092+ValentynAndrusik@users.noreply.github.com>
2019-10-29 02:18:26 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function getWeather(city = "Lviv") {
   return await fetch(
-    `https://api.weatherstack.com/current?access_key=128e9f062ce7ce0e464f0847488e6ce7&query=${city}`
+    `http://api.weatherstack.com/current?access_key=128e9f062ce7ce0e464f0847488e6ce7&query=${city}`
   );
 }
 
@@ -46,15 +46,17 @@ function getCity() {
   getWeather(city)
     .then(response => response.json())
     .then(data => {
+      console.log(data)
       for (keyData in data) {
-        // console.log(typeof keyData);
+        console.log(typeof keyData);
         if (keyData === "request") {
           for (keysInsideKeyData in data[keyData]) {
             const paragraph = document.createElement("p");
             paragraph.textContent = `${keysInsideKeyData}: ${data[keyData][keysInsideKeyData]}`;
             request.append(paragraph);
           }
-        } else if (keyData === "current") {
+        }
+         else if (keyData === "current") {
           for (keysInsideKeyData in data[keyData]) {
             const paragraph = document.createElement("p");
             paragraph.textContent = `${arr[i]}: ${data[keyData][keysInsideKeyData]}`;
@@ -66,37 +68,6 @@ function getCity() {
     });
 }
 
-// observation_time: 02:56 PM
-
-// temperature: 8
-
-// weather_code: 116
-
-// weather_icons: https://assets.weatherstack.com/images/wsymbols01_png_64/wsymbol_0002_sunny_intervals.png
-
-// weather_descriptions: Partly cloudy
-
-// wind_speed: 11
-
-// wind_degree: 280
-
-// wind_dir: W
-
-// pressure: 1018
-
-// precip: 0.5
-
-// humidity: 81
-
-// cloudcover: 75
-
-// feelslike: 6
-
-// uv_index: 3
-
-// visibility: 10
-
-// is_day: yes
 
 // getWeather(city)
 //     .then(response => response.json())
